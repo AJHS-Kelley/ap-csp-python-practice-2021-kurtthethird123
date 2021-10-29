b770ef44b5790f1ff03a254aef9af9b9c4279483
kurtthethird123 <90337249+kurtthethird123@users.noreply.github.com>
2021-10-29 13:21:03 -0400

    0.1

--- .github/variables/variables_practice.py
+++ .github/variables/variables_practice.py
@@ -6,3 +6,19 @@
 my_int = 75 # INTEGERS, any +/- whole number and 0
 my_float = 1.234567 # FLOATING POINT, any +/- number with a decimal.
 my_string = "Rest in peace IcemanEtika"
+
+print(my_int)
+print(my_float)
+print(my_string)
+
+my_int = input("Joyconboys for life/n")
+print(my_int)
+
+my_int = input("pay your respects/n")
+print(my_int)
+
+my_int = input("for the iceman/n")
+print(my_int)
+
+my_int = input("use joycons for life/n")
+print(my_int)
