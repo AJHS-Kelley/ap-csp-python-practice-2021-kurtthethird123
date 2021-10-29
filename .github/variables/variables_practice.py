436a95394398c9a981fd3dd3a991a1c37d9cb569
kurtthethird123 <90337249+kurtthethird123@users.noreply.github.com>
2021-10-29 13:03:01 -0400

    version 0.1
    added three variables and an interger

--- .github/variables/variables_practice.py
+++ .github/variables/variables_practice.py
@@ -1,1 +1,8 @@
-# Variables Practice, Tennell Curtis/lupin III, 10/29/21 12:49, version 0.0
+# Variables Practice, Tennell Curtis/lupin III, 10/29/21 1:01PM, version 0.1
+
+
+# Basic Variables 
+
+my_int = 75 # INTEGERS, any +/- whole number and 0
+my_float = 1.234567 # FLOATING POINT, any +/- number with a decimal.
+my_string = "Rest in peace IcemanEtika"
